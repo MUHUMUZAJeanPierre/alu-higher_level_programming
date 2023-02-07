85f82295d677b4f951a7e8778aa0a0f17e0b5175
MUHUMUZAJeanPierre <116554528+MUHUMUZAJeanPierre@users.noreply.github.com>
2023-02-07 13:47:16 +0200

    Update 7-islower.py

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-def uppercase(str):
-    new = ""
-    for char in str:
-        if ord('a') <= ord(char) <= ord('z'):
-            char = ord(char) - 32
-            new += chr(char)
-        else:
-            new += char
-    print("{}".format(new))
+
+
+def islower(c):
+
+        return ord(c) >= ord('a') and ord(c) <= ord('z')
