bef29b8613cd437aca89b36c36ae8fc007622b5a
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2023-03-29 05:22:07 -0700

    genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
-- just use ids 
+-- just use ids 
 SELECT tv_genres.name FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
