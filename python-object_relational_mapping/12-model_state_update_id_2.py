d0661ac6e7174711d328e4231e909fff90e8c5ef
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2023-04-21 02:48:53 -0700

    12-model_state_update_id_2.py

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""script that changes the name of a State object
-from the database hbtn_0e_6_usa
+"""
+Script that changes the name of a State object to the database
+Using module SQLAlchemy
 """
 
-import sys
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-
+from sqlalchemy.orm import sessionmaker
+from sys import argv
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # create an engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    # create a configured "Session" class
     Session = sessionmaker(bind=engine)
+    # create a Session
     session = Session()
     Base.metadata.create_all(engine)
-
-    state = session.query(State).filter(State.id == 2).first()
-
-    state.name = "New Mexico"
-
+    state_update = session.query(State).filter_by(id='2').first()
+    state_update.name = "New Mexico"
+    # commit and close session
     session.commit()
     session.close()
