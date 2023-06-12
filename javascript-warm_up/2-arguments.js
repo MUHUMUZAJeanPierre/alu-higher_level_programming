6959626369545b26856172b889496d4fdc932574
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2023-06-12 08:07:36 +0000

    this is script

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-const msg = process.msg.slice(2);
-if (msg.length === 0){
-	console.log('No argument');
-} if (msg.length === 1) {
-	console.log('Argumet found');
+
+const msg = process.argv.slice(2);
+
+if (msg.length === 0) {
+  console.log('No argument');
+} else if (msg.length === 1) {
+  console.log('Argument found');
 } else {
-	console.log('Argument found');
+  console.log('Arguments found');
 }
