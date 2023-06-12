ec724683a3b6dbcc472026bd8022e3e70a3bc27b
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2023-06-12 10:17:33 +0000

    THis NaT

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-const args = process.aargv[2];
+const args = process.argv[2];
 if (isNaN(args)) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number: ' +args);
+  console.log('My number: ' + args);
 }
