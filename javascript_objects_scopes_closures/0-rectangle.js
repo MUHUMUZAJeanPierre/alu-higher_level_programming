2da1b90af37d8557df848e30dc0a8429d93d86fc
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2023-06-14 13:08:18 +0000

    This is empty class clalled Rectangle

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 class Rectangle {
+	constructor(_width, _height){
+	this.width = width;
+	this.height = height;
+}
 }
 const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor);
