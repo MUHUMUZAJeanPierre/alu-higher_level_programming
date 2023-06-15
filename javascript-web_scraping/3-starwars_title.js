eb011a1f51dd614e5feb9c6650d859f9ae62b492
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2023-06-15 08:19:05 +0000

    fdofhdufgdbuf

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const id = process.argv[2];
-
-request.get(`https://swapi-api.alx-tools.com/api/films/${id}`, function (err, res) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(JSON.parse(res.body).title);
-  }
+request.get(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (err, response, body) => {
+  if (err) console.log(err);
+  console.log(JSON.parse(body).title);
 });
