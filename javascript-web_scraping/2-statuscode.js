7a9a01ed2a253b06e43af65480ac5577da942c48
MUHUMUZAJeanPierre <j.muhumuza@alustudent.com>
2023-06-15 08:03:54 +0000

    script taht display the status code of a GET request

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,6 @@
+i!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], (err, response) => {
+  if (err) console.log(err);
+  console.log(`code: ${response.statusCode}`);
+});
